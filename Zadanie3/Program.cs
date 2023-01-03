b6086c533ddc28795eb0fcaf228e2f746ff0fa21
rusikcrasava <rusikcrasava@mail.ru>
2023-01-03 14:20:47 +0300

    Третья задача выполнена

---
+++ Zadanie3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+    Console.Write("Введите положительное число: ");
+    int N = int.Parse(Console.ReadLine());
+    if (N<1)
+      {
+        Console.WriteLine("Вы ввели не положительное число");
+      }
+    else
+      {
+        Console.WriteLine($"таблица кубов от 1 до {N}: ");
+        int i=1; 
+        while (i<=N)
+          {
+            Console.Write($"{Math.Pow(i,3)} ");
+            i++;        
+          }
+      }
