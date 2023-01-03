f90d9d60443fa1d0a81a0561eb02a51e3e1e70ce
rusikcrasava <rusikcrasava@mail.ru>
2023-01-03 14:17:06 +0300

    Вторая задача выполнена

---
+++ Zadanie2/Program.cs
@@ -0,0 +1,14 @@
+﻿  Console.Write("Введите значение X первой точки: "); 
+    int X1 = int.Parse(Console.ReadLine());
+    Console.Write("Введите значение Y первой точки: "); 
+    int Y1 = int.Parse(Console.ReadLine());  
+    Console.Write("Введите значение Z первой точки: "); 
+    int Z1 = int.Parse(Console.ReadLine());      
+    Console.Write("Введите значение X второй точки: "); 
+    int X2 = int.Parse(Console.ReadLine());
+    Console.Write("Введите значение Y второй точки: "); 
+    int Y2 = int.Parse(Console.ReadLine()); 
+    Console.Write("Введите значение Z второй точки: "); 
+    int Z2 = int.Parse(Console.ReadLine());
+
+    Console.WriteLine($"Расстояние между точкой с координатами ({X1},{Y1},{Z1}) и точкой с координатами ({X2},{Y2},{Z2}) составляет: {Math.Sqrt(Math.Pow(X2-X1,2) + Math.Pow(Y2-Y1,2)+ Math.Pow(Z2-Z1,2)):f2}");
